bf1e9f0b4cfe4e7c8c7ecbf742912ecb210ab28c
Laura Luque <lauraluque1215@gmail.com>
2025-02-24 16:11:06 -0500

    refactor: :fire: remove store

--- src/index.js
+++ src/index.js
@@ -9,22 +9,11 @@ import './css/animate.css';
 import './css/flaticon.css';
 import './sass/style.scss';
 
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./store/index";
-import { Provider } from "react-redux";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
             <ParallaxProvider>
                 <App />
             </ParallaxProvider>
-        </PersistGate>
-    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
